8dab427c62a3e60f33f83d3f6c7be8d223db6725
DYLITAN09 <dylangutierrez441@gmail.com>
2022-08-29 15:11:05 -0500

    Conectando BBDD

--- model/DataBase.php
+++ model/DataBase.php
@@ -2,7 +2,7 @@
 	class DataBase{		
 		public function __construct(){}
 		public static function conexion(){
-			$pdo = new PDO('mysql:host=bsla2srwmwped7z4das1-mysql.services.clever-cloud.com;charset=utf8','ubzephhqm6y51uxk','FGbhprdAFTvyZcY5urZL');
+			$pdo = new PDO('mysql:host=bsla2srwmwped7z4das1-mysql.services.clever-cloud.com;charset=utf8','ubzephhqm6y51uxk1','FGbhprdAFTvyZcY5urZL1');
 			$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 			return $pdo;
 		}
